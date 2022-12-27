2ebaf1ec86ac57596314f01470e668430558a697
Mohammed uzair s <mohammeduzair333@gmail.com>
2022-12-27 16:04:22 +0530

    UUID Implementation

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,46 +6,24 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.UUID;
 
 @SpringBootApplication
 @RestController
 public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+
     }
 
     @PostMapping("/my-student")
     public OTP PostMyOTP(@RequestBody Student stu) {
-        OTP o=new OTP(getAlphaNumericString(20));
+        UUID uuid=UUID.randomUUID();
+        OTP o=new OTP(uuid.toString());
          return o;
 
     }
-    static String getAlphaNumericString(int n)
-    {
 
-        // chose a Character random from this String
-        String AlphaNumericString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-                + "0123456789"
-                + "abcdefghijklmnopqrstuvxyz";
-
-        // create StringBuffer size of AlphaNumericString
-        StringBuilder sb = new StringBuilder(n);
-
-        for (int i = 0; i < n; i++) {
-
-            // generate a random number between
-            // 0 to AlphaNumericString variable length
-            int index
-                    = (int)(AlphaNumericString.length()
-                    * Math.random());
-
-            // add Character one by one in end of sb
-            sb.append(AlphaNumericString
-                    .charAt(index));
-        }
-
-        return sb.toString();
-    }
 
 
 }
